688459bed00e3c8fc0364f74bab9824863387094
haroldCoder <haroldc2005@gmail.com>
2023-09-09 16:46:31 -0500

    xyz-banck-selenium

---
+++ src/App.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\koderx\\Desktop\\Nueva carpeta\\banco\\src\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login");
+
+        //darle tiempo de espera a la pagina
+
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        // Encuentra el elemento del botón
+        WebElement buttonCustomer = driver.findElement(By.xpath("//button[contains(@class, 'btn btn-primary btn-lg')]"));
+
+        // Haz clic en el botón
+        buttonCustomer.click();
+    }
+}

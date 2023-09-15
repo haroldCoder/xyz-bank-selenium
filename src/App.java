827e358f0c1bae1cd58eddb49a5e3394a2556c5a
haroldCoder <haroldc2005@gmail.com>
2023-09-15 11:51:03 -0500

    organizar codigo

--- src/App.java
+++ src/App.java
@@ -4,8 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.NoSuchElementException;
-
 import org.junit.*;
 
 public class App {
